841e6a22f025e02649379fa46229ab937e01ee02
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-04-15 13:26:36 +0700

    Testing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,6 @@
 <script lang="ts" setup>
 
 const colorMode = useColorMode()
-// const isDark = computed({
-//   get() {
-//     return colorMode.value === 'dark'
-//   },
-//   set() {
-//     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-//   },
-// })
 </script>
 
 <template>
