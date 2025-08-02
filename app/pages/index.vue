dfd66e2838ce50a47bc715f36330c203c0b725bd
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-08-03 01:40:30 +0700

    10th attempt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -390,7 +390,7 @@ onBeforeMount(async () => {
     },
   });
   console.log("Type", typeof video_thumbnail.value, video_thumbnail.value);
-  handleBlob(video_thumbnail.value);
+  handleBlob(video_thumbnail.value[0]);
 });
 
 // Initialize map zoom when component mounts
